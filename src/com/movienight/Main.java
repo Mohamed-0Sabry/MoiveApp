1009a0c9df1285a9dbf9986fa5300aa39341bfd0
Mohamed Sabry <mohamed.2520076@stemelsadat.moe.edu.eg>
2025-04-28 20:48:29 +0300

    Wipe everything

--- src/com/movienight/Main.java
+++
@@ -1,9 +0,0 @@
-package com.movienight;
-
-import javafx.application.Application;
-
-public class Main {
-    public static void main(String[] args) {
-        Application.launch(MovieNightApp.class, args);
-    }
-}
