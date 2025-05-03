9b736e130f55d58005729b8316a0b8bf09722f54
NoohaShehab <nohashehab576@gmail.com>
2025-05-03 18:18:11 +0300

    Full screen button and some fixes

---
+++ com/movieapp/DemoMain.java
@@ -0,0 +1,26 @@
+package com.movieapp;
+
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+
+public class DemoMain extends Application {
+    @Override
+    public void start(Stage primaryStage) throws Exception {
+        FXMLLoader loader = new FXMLLoader(getClass().getResource("view/ViewerScreen.fxml"));
+        Parent root = loader.load();
+        Scene scene = new Scene(root);
+        scene.getStylesheets().add(getClass().getResource("styles/viewer.css").toExternalForm());
+        primaryStage.setTitle("Movie Viewer Demo");
+        primaryStage.setScene(scene);
+        primaryStage.setWidth(900);
+        primaryStage.setHeight(600);
+        primaryStage.show();
+    }
+
+    public static void main(String[] args) {
+        launch(args);
+    }
+} 
